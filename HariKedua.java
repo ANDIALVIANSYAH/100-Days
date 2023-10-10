d930c19213af1f29ecfe835e7093ef6fa795f26d
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-10 08:00:12 +0800

    HariKedua.java

---
+++ HariKedua.java
@@ -0,0 +1,18 @@
+package HariKedua;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    String nama ="Alvin";
+    System.out.println("Nama saya \t\t: "+nama);
+        String alamat = "Tapalang";
+        System.out.println("Alamat saya ada di \t: "+alamat);
+     int umur = 17;
+        System.out.println("umur saya adalah \t:"+umur + "Tahun");
+        double tinggi =163.3;
+        System.out.println("Tinggi badan saya\t: "+tinggi);
+    char kelas ='A';    
+        System.out.println("Saya dari Informatika\t: "+kelas);
+    }
+    
+}
