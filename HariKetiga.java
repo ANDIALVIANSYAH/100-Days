dc8ba097373b92f1211bce52c68f0abc2ee83c61
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-11 10:11:41 +0800

    HariKetiga.java

---
+++ HariKetiga.java
@@ -0,0 +1,23 @@
+package aritmetika;
+public class Aritmetika {
+
+    
+    public static void main(String[] args) {
+      int panjang = 100;
+      int pendek = 50;
+      int penjumlahan = panjang + pendek;
+      int pengurangan = panjang - pendek;
+      int pembagian = panjang / pendek;
+      int perkalian = panjang * pendek;
+      int modulus = panjang % pendek;
+      
+        System.out.println("hasil dari "+panjang+" + "+pendek+" = "+penjumlahan);
+        System.out.println("hasil dari "+panjang+" - "+pendek+" = "+pengurangan);
+        System.out.println("hasil dari "+panjang+" / "+pendek+" = "+pembagian);
+        System.out.println("hasil dari "+panjang+" * "+pendek+" = "+perkalian);
+        System.out.println("hasil dari "+panjang+" % "+pendek+" = "+modulus);
+     
+      
+    }
+    
+}
