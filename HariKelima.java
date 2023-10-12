6b2d01606b057a9fbbf78ad7b87fd62c3728cffb
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-13 07:13:39 +0800

    HariKelima.java

---
+++ HariKelima.java
@@ -0,0 +1,15 @@
+package Happy.Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Perbandingan
+        int angka1 = 3;
+        int angka2 = 6;   
+        System.out.println(angka1 >= angka2);
+        System.out.println(angka2 < angka1);
+        System.out.println(angka2 > angka1);
+        System.out.println(angka1 == angka1);
+        System.out.println(angka2 != angka1);
+    }
+}
