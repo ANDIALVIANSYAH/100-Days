f069ca34f0a3e29b148b38ac7b5498f395f7d550
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-12 19:24:40 +0800

    HariKeEmpat.java

---
+++ HariKeEmpat.java
@@ -0,0 +1,12 @@
+package JavaAlvin;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka1 = 3;
+        int angka2 = 4;
+        System.out.println(angka1 += angka2);
+        
+    }
+    
+}
