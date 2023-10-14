96327a2b640072d6fa511b392fc9e5ea72fa25e2
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-14 10:26:16 +0800

    HarikeEnam.java

---
+++ HarikeEnam.java
@@ -0,0 +1,12 @@
+package HariKe6;
+
+public class HariKe6 {
+    
+    public static void main(String[] args) {
+        int nilai1 =30;
+        int nilai2 =10;
+        
+        System.out.println(nilai1 -= nilai2);
+    }
+    
+}
