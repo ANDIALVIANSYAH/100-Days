8ee4d3bd4c283015f21fb988585349bd76778c5f
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-15 07:03:34 +0800

    DayKetujuh.java

---
+++ DayKetujuh.java
@@ -0,0 +1,8 @@
+package Hari7;
+public class Hari7 {   
+    public static void main(String[] args) {
+       int angka = 11;
+        int angka1 = 10;   
+        System.out.println(angka *= angka1);
+    }  
+}
