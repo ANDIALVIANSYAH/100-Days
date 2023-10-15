5964bccb1eb4a4a31205927609b55ed8ad4b1044
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-16 06:41:19 +0800

    Daykedelapan.java

---
+++ Day kedelapan.java
@@ -0,0 +1,11 @@
+package Hari8;
+
+public class Hari7 {
+    
+    public static void main(String[] args) {
+       int angka = 20;
+        int angka1 = 10;
+        
+        System.out.println(angka /= angka1);
+    }  
+}
