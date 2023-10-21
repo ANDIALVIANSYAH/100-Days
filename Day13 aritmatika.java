9b5ba00d36363581cfa3a534ecbd014c40320d4d
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-21 20:06:27 +0800

    Day13 aritmatika.java

---
+++ Day13 aritmatika.java
@@ -0,0 +1,20 @@
+package alvinjava;
+public class AlvinJava {
+
+    public static void main(String[] args) {
+     //kesimpulannya adalah
+     //perkalian atau pembagia akan dilakukan terlebih dulu
+     
+     /*jika dilakukan perkalian dan pembagian,
+     maka akan dilakuakan operasinya dari kiri ke kanan
+     */
+     int hasil = 5*10;
+     System.out.println(hasil);
+     hasil = 5 + 2 -10;
+     System.out.println(hasil);
+     
+     //menggunakan pengelompokkan operasi dengan ( )
+     hasil =10 / (3 + 6);
+     System.out.println(hasil);
+    }
+}
