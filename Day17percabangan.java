b7ec24cfcfa72acdbf7d7f0d31ac06b38ff25c42
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-25 18:56:08 +0800

    Day17percabangan.java

---
+++ Day17percabangan.java
@@ -0,0 +1,18 @@
+package percabangan1;
+
+public class Main {
+    
+    public static void main(String[] args) {
+                int angka = 5;
+        System.out.println("nilai = "+angka);
+        if (angka > 6){
+         System.out.println("Betul");
+        System.out.println("selesai");
+    }else{
+        System.out.println("salah");
+        System.out.println("selesai");
+    }
+        
+    }
+    
+}
