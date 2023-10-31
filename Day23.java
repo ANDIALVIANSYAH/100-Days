dfe1f5a582b9e006540141fd8d6ff4ffd20b0c6d
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-10-31 19:37:48 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,19 @@
+package belajar1;
+
+public class Main {
+   // operator penugasan 
+    public static void main(String[] args) {
+        int a = 20;
+        a += a;
+        System.out.println(a);
+        a-=20;
+        System.out.println(a);
+        a*=a;
+        System.out.println(a);
+        a/=5;
+        System.out.println(a);
+        a%=5;
+        System.out.println(a+"\nKHAMSHAMNIDA:)");
+    }
+    
+}
