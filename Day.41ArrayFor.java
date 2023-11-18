30125e641c6dfc5609e8d8dfbd58c2dc0b0919e4
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-11-18 21:06:04 +0800

    Day.41ArrayFor.java

---
+++ Day.41ArrayFor.java
@@ -0,0 +1,14 @@
+package looparray;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Looping array dengan For Eanc
+        int[] Arrayangka ={1,2,3,4,5,6,7,8,9,10};
+        System.out.println("looping Standard");
+        for(int i = 0; i < 10; i++){
+           System.out.println("index ke- "+i+" adalah = "+ Arrayangka[i]); 
+        }
+    }
+    
+}
