e7b04e970eccc6e4dac62b9ddc16ed0929fa1c48
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-11-19 21:54:16 +0800

    Day.42.java

---
+++ Day.42.java
@@ -0,0 +1,21 @@
+package day42;
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner alvin = new Scanner(System.in);
+        int n;
+        System.out.print(" masukan nilai n : ");
+        n = alvin.nextInt();
+         int array [] = {1,5,3,5,7};
+         for (int i = 0; i < array.length; i++) {
+             System.out.println(" masukan nilai index ke - "+(i+1)+" : "+array[i]);
+            
+        }
+            System.out.println("[1,5,3,5,7]");
+        }
+        
+        
+    }
+
+              
