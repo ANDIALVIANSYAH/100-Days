e732402b6cf0ca17da21bdad7c70711935820a65
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-11-23 20:39:22 +0800

    Day46.java

---
+++ Day46.java
@@ -0,0 +1,20 @@
+package belajararray;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Assigment
+        // tipe data [] nama = (komponen-komponen)
+        System.out.println("Assigment Array");
+        // index array selalu dimulai dari 0
+        //           index = 0  1  2   3
+        //                   |  |  |   |
+        String [] arrayangka = {"Alvin","Andi","rizky","jeno"};
+        
+        System.out.println(arrayangka[0]);
+        System.out.println(arrayangka[1]);
+        System.out.println(arrayangka[2]);
+        System.out.println(arrayangka[3]);
+    }
+    
+          }
