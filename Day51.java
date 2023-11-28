b1ef55d5acbf7026160ea94dbff739fad0a92330
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-11-28 21:42:15 +0800

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,15 @@
+package LatihanBelajar1;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //buatlah perulangan dari 1-10 dan jumlahkan!.
+        int hasil = 0;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+            hasil += i;
+        }
+        System.out.println("hasilnya ialah : "+hasil);
+    }
+    
+}
