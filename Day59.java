5938d4566cf75f4217ea88bdd9510573c5e1c43b
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-12-06 21:05:41 +0800

    Day59.java

---
+++ Day59.java
@@ -0,0 +1,15 @@
+package Day100ofcoding6;
+
+public class Day100ofcoding6 {
+    
+    public static void main(String[] args) {
+        // Pak bayu ingin membuat program yang menyimpan nama-nama mahasiswa dengan nilai tertinggi.
+        //Berikut adalah daftar nama mahasiswa dengan nilai tertinggi (Andi, Alvin, Jeno, Rizky, Abdi.) 
+        //(simpan dalam array)
+        String nama[] = {"Andi ","Alvin ","Jeno ","Rizky ","Abdi "};
+        for(int i = 0; i < nama.length; i++){
+            System.out.println(i + 1+" "+nama[i]);
+        }
+    }
+    
+}
