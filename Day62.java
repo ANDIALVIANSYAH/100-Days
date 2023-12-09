3e450897ee82bf9292035f05b6ca50e4ee766256
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-12-09 22:02:32 +0800

    Day62.java

---
+++ Day62.java
@@ -0,0 +1,14 @@
+package Day100ofcoding10;
+
+public class Day100ofcoding9 {
+    public static void main(String[] args) {
+        cetakAngka();
+    }
+
+    // Fungsi untuk mencetak angka 1 hingga 10 dengan perulangan for
+    static void cetakAngka() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
