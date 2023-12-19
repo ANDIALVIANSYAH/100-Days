776d4c04c3ae4549236b05a65167aefe06f8d9af
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-12-19 15:18:51 +0800

    Day72.java

---
+++ Day72.java
@@ -0,0 +1,22 @@
+package Day100ofcoding18;
+
+public class Day100ofcoding18 {
+    
+    public static void main(String[] args) {
+        // Panggil fungsi yang menggunakan while loop
+        myWhileLoopFunction();
+    }
+
+    // Fungsi dengan while loop
+    public static void myWhileLoopFunction() {
+        int i = 1;
+
+        // While loop untuk mencetak angka 1 hingga 5
+        while (i <= 5) {
+            System.out.println("Angka: " + i);
+            i++;
+        }
+    }
+}
+
+    
