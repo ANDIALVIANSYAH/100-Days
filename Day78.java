60493de16d7e646ff95a7d6fe39d27f38348e819
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-12-25 20:03:59 +0800

    Day78.java

---
+++ Day78.java
@@ -0,0 +1,21 @@
+package java6;
+import java.util.Scanner;
+public class java6 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bil,pangkat,hasil;
+        System.out.print("Bilangan : ");
+        bil = input.nextInt();
+        System.out.print("Pangkat  : ");
+        pangkat = input.nextInt();
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.print("Hasil    : ");
+        hasil = bil;
+        
+        for (int i = 1; i < pangkat; i++){
+            hasil *= bil;
+    }
+        System.out.println(hasil);
+    }
+}
