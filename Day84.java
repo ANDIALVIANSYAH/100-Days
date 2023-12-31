6e56ecd092874eb0589431fb63e20a7ff917f2b1
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2023-12-31 19:17:52 +0800

    Day84.java

---
+++ Day84.java
@@ -0,0 +1,13 @@
+package java11;
+import java.util.Scanner;
+public class java11 {
+    
+    public static void main(String[] args) {
+        Scanner up = new Scanner(System.in);
+        System.out.print("Masukan kata : ");
+        String kata = up.nextLine();
+        System.out.println("________________________");
+        System.out.println("kata awal       : "+kata);
+        System.out.println("hasil konversi  : "+kata.toUpperCase()+"\nMakasih : )");
+    }
+}
