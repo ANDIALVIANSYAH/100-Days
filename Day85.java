a38a7a5cb4b3e8d1d6923f958c6dc3bb93815724
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2024-01-01 19:15:35 +0800

    Day85.java

---
+++ Day85.java
@@ -0,0 +1,15 @@
+package java12;
+import java.util.Scanner;
+public class java12 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner ( System.in);
+        System.out.println("Table penjumlahan");
+        System.out.print("Masukka angka : ");
+        int angka = input.nextInt();
+        
+     for (int i = 0; i < angka; i++){
+    System.out.println(angka + "+"+ (i+1)+" = "+(angka+(i+1)));
+       }
+    }
+}
