89aad28e9b200a6c0898ec8a39db4b223b67b272
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2024-01-11 19:19:27 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,19 @@
+package java19;
+import java.util.Scanner;
+public class java19 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int jumlah = 0;
+
+        System.out.print("Masukkan batas suatu bilangan: ");
+        int batas = input.nextInt();
+
+        for (int i = 1; i <= batas; i++) {
+            System.out.print(i+" ");
+            jumlah += i;
+        }
+
+        System.out.println("Jumlah dari total bilangan di atas adalah: "+jumlah);
+    }
+}
